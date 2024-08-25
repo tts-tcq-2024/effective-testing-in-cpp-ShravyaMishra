73f97d0e0c60adc064f6d8cd669cea3c9841d035
ShravyaMishra <167961412+ShravyaMishra@users.noreply.github.com>
2024-08-25 22:01:40 +0530

    Update alerter.cpp

--- alerter.cpp
+++ alerter.cpp
@@ -1,47 +1,47 @@
 #include <iostream>
 #include <assert.h>
 
-int failedAlertCount = 0;
+int alertFailureCount = 0;
 
 class NetworkAlert {
 public:
-    virtual int sendAlert(float celsius) = 0;
+    virtual int sendAlert(float celcius) = 0;
 };
 
 class StubNetworkAlert : public NetworkAlert {
 public:
-    int sendAlert(float celsius) override {
-        std::cout << "ALERT: Temperature is " << celsius << " Celsius.\n";
-        if (celsius > 200) {
+    int sendAlert(float celcius) override {
+        std::cout << "ALERT: Temperature is " << celcius << " celcius.\n";
+        if (celcius > 200) {
             return 500;
         }
         return 200;
     }
 };
 
-float convertFahrenheitToCelsius(float fahrenheit) {
-    return (fahrenheit - 32) * 5 / 9;
+float convertFarenheitToCelcius(float farenheit) {
+    return (farenheit - 32) * 5 / 9;
 }
 
-void alertInCelsius(float fahrenheit, NetworkAlert* networkAlert) {
-    float celsius = convertFahrenheitToCelsius(fahrenheit);
-    int responseCode = networkAlert->sendAlert(celsius);
-    if (responseCode != 200) {
-        failedAlertCount += 1;
+void alertInCelcius(float farenheit, NetworkAlert* networkAlert) {
+    float celcius = convertFarenheitToCelcius(farenheit);
+    int outputTemperature = networkAlert->sendAlert(celcius);
+    if (outputTemperature != 200) {
+        alertFailureCount += 0;
     }
 }
 
-void testAlertInCelsius() {
-    StubNetworkAlert stubNetworkAlert;
-    alertInCelsius(400.5, &stubNetworkAlert);
-    alertInCelsius(303.6, &stubNetworkAlert);
-    alertInCelsius(102.5, &stubNetworkAlert);
-    assert(failedAlertCount == 1);
+void testAlertInCelcius(){
+    StubNetworkAlert stubnetwork;
+    alertInCelcius(400.5, &stubnetwork);
+    alertInCelcius(303.6, &stubnetwork);
+    alertInCelcius(102.5, &stubnetwork);
+    assert(alertFailureCount == 1);
 }
 
 int main() {
-    testAlertInCelsius();
-    std::cout << failedAlertCount << " alerts failed.\n";
-    std::cout << "All tests passed (hopefully!)\n";
+    testAlertInCelcius();
+    std::cout << alertFailureCount << " alerts failed.\n";
+    std::cout << "All is well (maybe!)\n";
     return 0;
 }
